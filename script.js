3ecb4fb5a6069b7bb514cca9d669d53c3b87fa45
Pedroarthur1999 <pedroarthur1999@gmail.com>
2022-01-07 02:00:04 -0300

    V .Last

--- script.js
+++ script.js
@@ -1,47 +1,35 @@
+const relogio = document.querySelector(".relogio p");
+const inicio = document.querySelector("#start");
+const pausa = document.querySelector("#stop");
+const resetar = document.querySelector(".reset");
+let seconds = 0;
+let timer;
 
-
-function createHour(seconds){
-
-   const data = new Date(seconds * 1000);
-   return data.toLocaleTimeString('pt-br',{
-       hour12:false,
-       timeZone:'GMT'
-   })
+function createHour(seconds) {
+  const data = new Date(seconds * 1000);
+  return data.toLocaleTimeString("pt-br", {
+    hour12: false,
+    timeZone: "GMT",
+  });
 }
-
-let seconds = 0
-function countHours(){
-    
-    const timer = setInterval(function(){
-          
-        seconds++;
-        relogio.innerHTML = createHour(seconds)
-        relogio.style.color = 'black'
-    }, 1000)
-
-
-    
+function countHours() {
+  timer = setInterval(function () {
+    seconds++;
+    relogio.innerHTML = createHour(seconds);
+    relogio.style.color = "black";
+  }, 1000);
 }
-
-console.log(createHour(seconds));
-const relogio = document.querySelector('.relogio p')
-const inicio = document.querySelector('#start')
-const pausa = document.querySelector('#stop')
-const resetar = document.querySelector('.reset')
-
-inicio.addEventListener('click', function(event){
-    
-    countHours(seconds);
-    
+inicio.addEventListener("click", function (event) {
+  countHours(seconds);
 });
 
-pausa.addEventListener('click', function(event){
-    
-    
-    relogio.style.color = 'red'
+pausa.addEventListener("click", function (event) {
+  clearInterval(timer);
+  relogio.style.color = "red";
 });
 
-resetar.addEventListener('click', function(event){
-    
-    relogio.style.color = 'red'
+resetar.addEventListener("click", function (event) {
+  seconds = 0;
+  clearInterval(timer);
+  relogio.innerHTML = createHour(seconds);
 });
