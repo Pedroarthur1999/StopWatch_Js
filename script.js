63aa1f08d25572ebd8148aedeff48ca22de3f39e
Pedroarthur1999 <pedroarthur1999@gmail.com>
2022-01-07 01:16:45 -0300

    V 2.0

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+
+
+function createHour(seconds){
+
+   const data = new Date(seconds * 1000);
+   return data.toLocaleTimeString('pt-br',{
+       hour12:false,
+       timeZone:'GMT'
+   })
+}
+
+let seconds = 0
+function countHours(){
+    
+    const timer = setInterval(function(){
+          
+        seconds++;
+        relogio.innerHTML = createHour(seconds)
+        relogio.style.color = 'black'
+    }, 1000)
+
+
+    
+}
+
+console.log(createHour(seconds));
 const relogio = document.querySelector('.relogio p')
 const inicio = document.querySelector('#start')
 const pausa = document.querySelector('#stop')
@@ -5,7 +31,8 @@ const resetar = document.querySelector('.reset')
 
 inicio.addEventListener('click', function(event){
     
-    relogio.style.color = 'black'
+    countHours(seconds);
+    
 });
 
 pausa.addEventListener('click', function(event){
@@ -15,7 +42,6 @@ pausa.addEventListener('click', function(event){
 });
 
 resetar.addEventListener('click', function(event){
-    
     
     relogio.style.color = 'red'
 });
